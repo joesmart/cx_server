b4080f3eba360ddc12fa68df2c59349d91357431
jinhui <jinhui@f47374ae-05c8-11e1-8c5d-33d49f259814>
2012-08-03 02:42:04 +0000

    add suggestion interface
    git-svn-id: https://clsvn.cienet.com.cn/hzkx-caixiang/code/trunk/Phase3/CXServer_3.0/trunk@52 f47374ae-05c8-11e1-8c5d-33d49f259814

--- bussiness/src/main/java/com/server/cx/webservice/rs/server/SuggestionResource.java
+++ bussiness/src/main/java/com/server/cx/webservice/rs/server/SuggestionResource.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.cl.cx.platform.dto.OperationDescription;
+import com.cl.cx.platform.dto.SuggestionDTO;
 import com.server.cx.constants.Constants;
 import com.server.cx.entity.cx.Suggestion;
 import com.server.cx.exception.SystemException;
@@ -52,13 +53,8 @@ public class SuggestionResource {
         if (suggestions == null || suggestions.isEmpty()) {
             return Response.noContent().build();
         }
-        hideSuggestionUserInfo(suggestions);
-        return Response.ok(suggestions).build();
-    }
-
-    private void hideSuggestionUserInfo(List<Suggestion> suggestions) {
-        for(Suggestion suggestion : suggestions) {
-            suggestion.setUserInfo(null);
-        }
+//        hideSuggestionUserInfo(suggestions);
+        List<SuggestionDTO> suggestionDTOs = ObjectFactory.buildAllSuggestionDTOFromSuggestion(suggestions);
+        return Response.ok(suggestionDTOs).build();
     }
 }
