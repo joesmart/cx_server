fe5998940ee2cad0c87ea4ad6b7f097a709b8800
joesmart <zouyanjian110@gmail.com>
2012-09-05 13:23:17 +0800

    update 1.let maven only need to modify the application.local.properties and war anywhere  2.publish the UserInfo update restful interface;

--- bussiness/src/main/java/com/server/cx/webservice/rs/server/UserInfoResources.java
+++ bussiness/src/main/java/com/server/cx/webservice/rs/server/UserInfoResources.java
@@ -1,12 +1,15 @@
 package com.server.cx.webservice.rs.server;
 
+import com.cl.cx.platform.dto.RegisterDTO;
+import com.server.cx.exception.SystemException;
+import com.server.cx.service.cx.RegisterService;
+import com.server.cx.util.business.ValidationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,7 +25,10 @@ import javax.ws.rs.core.Response;
 @Produces({MediaType.APPLICATION_XML})
 public class UserInfoResources {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(UserInfoResources.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserInfoResources.class);
+    @Autowired
+    private RegisterService registerService;
+
     @PUT
     public Response updatePhoneNo(@PathParam("imsi")String imsi,String phoneNo){
         LOGGER.info(phoneNo);
@@ -31,13 +37,17 @@ public class UserInfoResources {
     }
 
     @POST
-    public Response update(@PathParam("imsi")String imsi,String phoneNo,@Context HttpServletRequest request,@HeaderParam("imsi") String userAgent){
-        LOGGER.info(phoneNo);
-        LOGGER.info(imsi);
-        while (request.getParameterNames().hasMoreElements()){
-            LOGGER.info(String.valueOf(request.getParameterNames().nextElement()));
+    public Response update(@PathParam("imsi")String imsi,@HeaderParam("phoneNo") String phoneNo){
+
+        try {
+            ValidationUtil.checkParametersNotNull(imsi,phoneNo);
+            RegisterDTO registerDTO = new RegisterDTO();
+            registerDTO.setImsi(imsi);
+            registerDTO.setPhoneNo(phoneNo);
+            registerService.update(registerDTO);
+        } catch (SystemException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
-        LOGGER.info(request.getHeader("imsi"));
         return Response.ok().build();
     }
 
