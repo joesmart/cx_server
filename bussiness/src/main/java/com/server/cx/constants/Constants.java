8ec82b486c855f9356933e5a3c79e58ee09ed13f
joesmart <zouyanjian110@gmail.com>
2012-09-11 16:18:17 +0800

    updated

--- bussiness/src/main/java/com/server/cx/constants/Constants.java
+++ bussiness/src/main/java/com/server/cx/constants/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String ERROR_FLAG = "ERROR";
 
     public static final String APP_IS_NEWEST = "APP_IS_UP_TO_DATE";
-    public static final String SERVER_HAVE_NEW_VERSION = "SERVER_HAVE_NEW_VERSION";
+    public static final String SERVER_HAVE_NEW_VERSION = "SERVER_HAVE_NEWVERION";
 
     // 短信内容
     public static final String SMS_CONTENT = "你的好友(?)正在使用手机彩像服务" + "非常好玩，可以定制个性的通话图像，" + "只需一秒钟就可以，一定要推荐给您!";
