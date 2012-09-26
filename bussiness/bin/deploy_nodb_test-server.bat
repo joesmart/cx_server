3925a671f9ddf76d64919e631258d08a722c7c55
joesmart <zouyanjian110@gmail.com>
2012-09-26 16:55:42 +0800

    Updated

--- bussiness/bin/deploy_nodb_test-server.bat
+++ bussiness/bin/deploy_nodb_test-server.bat
@@ -4,7 +4,6 @@ echo [INFO] Re-create the schema and provision the sample data.
 cd %~dp0
 cd ..
 
-call mvn clean compile war:war -Dproduct_ip=10.90.3.122 -Dapp_name=callface -Dproduct_port=38183 -Dproduct_resource_ip=10.90.3.122 -Dproduct_resource_port=38183
-
+call mvn clean compile war:war  -Dapp_name=callface
 cd bin
 pause
