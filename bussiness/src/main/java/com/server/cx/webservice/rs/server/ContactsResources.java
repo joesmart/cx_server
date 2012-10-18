8da0a9aed3140bd1bbb88eacabfd36b01ae1409d
jinhui <jinhui@f47374ae-05c8-11e1-8c5d-33d49f259814>
2012-10-18 04:29:37 +0000

    add log and try catch method
    git-svn-id: https://clsvn.cienet.com.cn/hzkx-caixiang/code/trunk/Phase3/CXServer_3.0/trunk@1194 f47374ae-05c8-11e1-8c5d-33d49f259814

--- bussiness/src/main/java/com/server/cx/webservice/rs/server/ContactsResources.java
+++ bussiness/src/main/java/com/server/cx/webservice/rs/server/ContactsResources.java
@@ -1,5 +1,18 @@
 package com.server.cx.webservice.rs.server;
 
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import com.cl.cx.platform.dto.ContactInfoDTO;
 import com.cl.cx.platform.dto.ContactsDTO;
 import com.cl.cx.platform.dto.OperationDescription;
@@ -9,20 +22,14 @@ import com.server.cx.service.cx.ContactsService;
 import com.server.cx.service.util.BusinessFunctions;
 import com.server.cx.util.ObjectFactory;
 import com.server.cx.util.business.ValidationUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Component
 @Path("{imsi}/contacts")
 //@Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
 public class ContactsResources extends OperationResources {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContactsResources.class);
+
     @Autowired
     private ContactsService contactsService;
 
@@ -32,15 +39,17 @@ public class ContactsResources extends OperationResources {
     @SuppressWarnings("finally")
     @POST
     public Response uploadContacts(@PathParam("imsi") String imsi, ContactsDTO uploadContactDTO) {
+        LOGGER.info("Into uploadContacts imsi = " + imsi);
+        LOGGER.info("uploadContactDTO = " + uploadContactDTO);
         OperationDescription operationDescription = new OperationDescription();
         try {
             ValidationUtil.checkParametersNotNull(uploadContactDTO);
             contactsService.uploadContacts(uploadContactDTO.getContactInfos(), imsi);
             operationDescription = ObjectFactory.buildOperationDescription(HttpServletResponse.SC_CREATED,
-                    "uploadContacts");
+                "uploadContacts");
         } catch (Exception e) {
             operationDescription = ObjectFactory.buildErrorOperationDescription(
-                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "uploadContacts", e.getMessage());
+                HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "uploadContacts", e.getMessage());
             return Response.ok(operationDescription).build();
         }
         return Response.ok(operationDescription).build();
@@ -48,12 +57,13 @@ public class ContactsResources extends OperationResources {
 
     @GET
     public ContactsDTO getContactsByImsi(@PathParam("imsi") String imsi) {
+        LOGGER.info("Into uploadContacts imsi = " + imsi);
         operationDescription = new OperationDescription();
         try {
             List<Contacts> contacts = contactsService.queryCXAppContactsByImsi(imsi);
             ContactsDTO contactDTO = new ContactsDTO();
             List<ContactInfoDTO> contactInfoDTOList = Lists.transform(contacts,
-                    businessFunctions.contactsTransformToContactInfoDTO());
+                businessFunctions.contactsTransformToContactInfoDTO());
             contactDTO.setContactInfos(contactInfoDTOList);
             return contactDTO;
         } catch (Exception e) {
